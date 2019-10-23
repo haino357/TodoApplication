e0fc5ba1f65b8fa360df2ce210b5edc08b399495
shouji takayuki <shouji_ta@nanoconnect.co.jp>
2019-10-23 13:47:02 +0900

    [change]:title change
    MainActivity & activity_mai name change

--- app/src/main/java/com/example/todoapplication/TasksActivity.kt
+++ app/src/main/java/com/example/todoapplication/TasksActivity.kt
@@ -1,12 +1,12 @@
 package com.example.todoapplication
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class TasksActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.tasks_act)
     }
 }
